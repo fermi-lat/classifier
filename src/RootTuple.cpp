46783f460b10a32fd3cc59c0b08c5c4cdde874a3
burnett <>
2005-07-28 20:38:20 +0000

    throw out bad input data.

--- src/RootTuple.cpp
+++ src/RootTuple.cpp
@@ -1,7 +1,7 @@
 /** @file RootTuple.cpp
 @brief implementation of RootTuple, RootTuple::Entry
 
-$Header: /cvsroot/d0cvs/classifier/src/RootTuple.cpp,v 1.8 2005/02/17 20:49:47 gwatts Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/classifier/src/RootTuple.cpp,v 1.1.1.1 2005/07/03 21:31:35 burnett Exp $
 */
 #include "classifier/RootTuple.h"
 #include "TFile.h"
@@ -36,7 +36,22 @@ namespace {
             }
         }
     }
-}
+#ifdef WIN32
+#include <float.h> // used to check for NaN
+#else
+#include <cmath>
+#endif
+
+    bool isFinite(double val) {
+        using namespace std; // should allow either std::isfinite or ::isfinite
+#ifdef WIN32 
+        return (_finite(val)!=0);  // Win32 call available in float.h
+#else
+        return (isfinite(val)!=0); // gcc call available in math.h 
+#endif
+    }
+
+}// anon namespace
 
 /** @class RootTuple::Entry
 @brief nested class is a functor to return the current value of an entry in the tree 
@@ -155,7 +170,12 @@ void RootTuple::fill_row(int event, std::vector<float>& row )const
     EntryList::const_iterator et=m_entries[i].begin();
     for(; et !=m_entries[i].end(); ++et){
         const Entry& e = **et;
-        row.push_back( e() );
+        float v = e();
+        if( !isFinite(v) ){
+            throw std::runtime_error("RootTuple::fill_row: Nan found in input");
+
+        }
+        row.push_back(v );
     }
 }
 
