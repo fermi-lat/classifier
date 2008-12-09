2ac52dd258ca456165ad2231cc5617aacb5c47ba
ecephas <>
2008-12-09 00:08:42 +0000

    Check in classifierLib.py for classifier v1r6p1 (branch-tag, GlastReleasev15r49).

--- classifierLib.py
+++ classifierLib.py
@@ -1,7 +1,7 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/classifier/classifierLib.py,v 1.1 2008/08/15 21:42:38 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['classifier'])
-
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
